a3758c3b873d9b74632128b4a31361d437011f7b
FullGauss <mihaserg1234@gmail.com>
2021-05-02 19:41:53 +0700

    create attribute tree app

--- src/components/TreeElement.vue
+++ src/components/TreeElement.vue
@@ -1,20 +1,37 @@
 <template>
-    <button @click="createNewAttribute">Добавить атрибут</button>
-    <button @click="createNewSubElement">Добавить элемент</button>
-    <div v-if="checkCanElementsRender( localAttributes )">
-        <input
-            v-for="attribute in localAttributes"
-            :key="attribute.id"
-            v-model="attribute.text"
-            type="text" >
-    </div>
-    <div v-if="checkCanElementsRender( localElements )">
-        <TreeElement 
-            v-for="element in localElements" 
-            :key="element"
-            :attributes="element.attributes"
-            :elements="element.elements"
-        />
+    <div class="tree-element">
+        <div class="attributes-panel">
+            <div class="attribute"
+                v-for="(attribute, attributeId) in localAttributes"
+                :key="attribute.id"
+            >
+                <input
+                    v-model="attribute.text"
+                    type="text" >
+                <button class="delete-button"
+                        @click="deleteAttribute(attributeId)"
+                >
+                </button>
+            </div>
+            
+        </div>
+        <div class="buttons-panel">
+            <button @click="createNewAttribute">Добавить запись</button>
+            <button @click="createNewSubElement">Добавить элемент</button>
+        </div>
+        <div class="subelements-panel" v-if="checkCanElementsRender( localElements )">
+            <div 
+                class="sub-element"
+                v-for="(element, elementId) in localElements" 
+                :key="element"
+            >
+                <TreeElement 
+                    :attributes="element.attributes"
+                    :elements="element.elements"
+                />
+                <button @click="deleteElement(elementId)" class="delete-element-button"></button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -58,15 +75,25 @@ export default {
                 elements: []
             };
             elements.value.push(newElement);
-        }
+        };
+
+        const deleteAttribute = (attributeNumber) => {
+            localAttributes.value.splice(attributeNumber, 1)
+        };
+        const deleteElement = (elementNumber) => {
+            localElements.value.splice(elementNumber, 1)
+        };
 
         return {
             localAttributes,
             localElements,
             createNewAttribute,
             checkCanElementsRender,
-            createNewSubElement
+            createNewSubElement,
+            deleteAttribute,
+            deleteElement
         }
     },
 }
 </script>
+<style scoped lang="scss" src="../assets/scss/treeElement.scss"></style>
--- src/views/ElementTreeView.vue
+++ src/views/ElementTreeView.vue
@@ -1,14 +1,26 @@
 <template>
-    <article v-if="checkCanElementsRender( elementTree.elements )">
-        <TreeElement 
-            v-for="element in elementTree.elements" 
-            :key="element"
-            :attributes="element.attributes"
-            :elements="element.elements"
-        />
+    <article class="editor">
+        <section class="editor-header">
+            <div class="buttons-panel">
+                <button @click="createNewTreeElement">Добавить элемент</button>
+                <button @click="show">Посмотреть результат</button>
+            </div>
+             <h1 class="title">Дерево записей</h1>
+        </section>
+        <section class="editor-workspace" v-if="checkCanElementsRender( elementTree.elements )">
+            <div 
+                class="element-root"
+                v-for="(element, elementId) in elementTree.elements" 
+                :key="element"
+            >
+                <TreeElement 
+                    :attributes="element.attributes"
+                    :elements="element.elements"
+                />
+                <button @click="deleteElement(elementId)" class="delete-element-button"></button>
+            </div>
+        </section>
     </article>
-    <button @click="createNewTreeElement">Добавить элемент</button>
-    <button @click="show">Посмотреть результат</button>
 </template>
 
 <script>
@@ -41,7 +53,11 @@ export default {
         },
         show() {
             console.log(this.elementTree);
+        },
+        deleteElement(elementId){
+            this.elementTree.elements.splice(elementId, 1)
         }
     }
 }
 </script>
+<style scoped lang="scss" src="./ElementTreeView.scss"></style>
